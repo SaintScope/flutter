498db421782b85f69434d32d94c711590b67381a
Connor Love <connorlove9@gmail.com>
2023-04-20 12:31:43 +0100

    modified:   dev/benchmarks/test_apps/stocks/test_driver/stock_view.dart 	modified:   dev/integration_tests/ios_add2app_life_cycle/flutterapp/lib/main.dart

--- dev/benchmarks/test_apps/stocks/test_driver/stock_view.dart
+++ dev/benchmarks/test_apps/stocks/test_driver/stock_view.dart
@@ -2,10 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// ignore_for_file: avoid_dynamic_calls
+
 import 'package:flutter_driver/driver_extension.dart';
 import 'package:stocks/main.dart' as app;
 
 void main() {
   enableFlutterDriverExtension();
   app.main();
+}
+
+void enableFlutterDriverExtension() {
 }
